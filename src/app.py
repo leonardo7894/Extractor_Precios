d909ae45253c191308bcf1710d0be5d9976868ce
leonardo ojeda <nahuelojeda2007@gmail.com>
2025-11-17 17:21:41 -0300

    hice el estractor de preciossssss

--- src/app.py
+++ src/app.py
@@ -3,61 +3,80 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
-import pandas as pd
 import re
+import pandas as pd
+
+
 
 def limpiar_texto(texto):
-    if texto:
-        return texto.replace("\xa0", " ").replace("\n", " ").strip()
-    return ""
+   if texto:
+       return texto.replace("\xa0", " ").replace("\n", " ").strip()
+   return ""
+
 
 def extraer_productos(url: str):
-    productos = []
+   productos = []
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
+   chrome_options = Options()
+   chrome_options.add_argument("--headless")
+   chrome_options.add_argument("--no-sandbox")
+   chrome_options.add_argument("--disable-gpu")
 
-    service = Service("/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    print(f"Intentando extraer datos de: {url}\n")
-    driver.get(url)
-    time.sleep(10)  # esperar que cargue JS
+   service = Service("/usr/bin/chromedriver")
+   driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    # Scroll para cargar más productos
-    SCROLL_PAUSA = 2
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(SCROLL_PAUSA)
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
 
-    time.sleep(5)
-    html = driver.page_source
-    driver.quit()
-    soup = BeautifulSoup(html, "html.parser")
+   print(f"Intentando extraer datos de: {url}\n")
+   driver.get(url)
+   time.sleep(10)  # esperar que cargue JS
 
-    for producto in soup.find_all("article"):
-        titulo_span = producto.select_one("h3")
-        precio_span = producto.select_one("div") 
 
-        if titulo_span and precio_span:
-            titulo = limpiar_texto(titulo_span.get_text())
-            precio = limpiar_texto(precio_span.get_text())
-            productos.append({"titulo": titulo, "precio": precio})
-        else:
-            print("No se pudieron extraer datos de un producto")
+   # Scroll para cargar más productos
+   SCROLL_PAUSA = 2
+   last_height = driver.execute_script("return document.body.scrollHeight")
+   while True:
+       driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+       time.sleep(SCROLL_PAUSA)
+       new_height = driver.execute_script("return document.body.scrollHeight")
+       if new_height == last_height:
+           break
+       last_height = new_height
 
-    print(f"Se extrajeron {len(productos)} productos")
-    return productos
 
-def extraer_precio(precio_texto):
-    match = re.search(r'(\d{1,3}(?:\.\d{3})*,\d{2})', precio_texto)
-    if match:
-        return match.group(1)
-    return None
+   time.sleep(5)
+   html = driver.page_source
+   driver.quit()
+   soup = BeautifulSoup(html, "html.parser")
+
+
+   for producto in soup.find_all("article"):
+       titulo_span = producto.select_one("h3")
+       precio_span = producto.select_one("div")
+
+
+       if titulo_span and precio_span:
+           titulo = limpiar_texto(titulo_span.get_text())
+           precio = limpiar_texto(precio_span.get_text())
+           productos.append({"titulo": titulo, "precio": precio})
+       else:
+           print("No se pudieron extraer datos de un producto")
+           
+   df = pd.DataFrame(productos)
+   df.to_csv("productos.csv", index=False, encoding="utf-8-sig")
+   print(f"CSV generado con {len(df)} productos")
+
+   # Convertir a DataFrame y guardar CSV
+   df = pd.DataFrame(productos)
+   df.to_csv("productos_limpios.csv", index=False, encoding="utf-8-sig")
+   print(f"CSV generado con {len(df)} productos")
+
+   print(f"Se extrajeron {len(productos)} productos")
+   return productos
+
+
+def precio_limpio(precio_texto):
+   match = re.search(r'(\d{1,3}(?:\.\d{3})*,\d{2})', precio_texto)
+   if match:
+       return match.group(1)
+   return None
--- tests/precio_test.py
+++ tests/precio_test.py
@@ -1,24 +1,16 @@
-from src.app import extraer_productos, extraer_precio
-import pandas as pd
+from src.app import extraer_productos
+import os
+
+
 
 def test_precio():
-    datos = extraer_productos("https://diaonline.supermercadosdia.com.ar/")
-    print(datos)
-
-    productos_filtrados = []
-    for p in datos:
-        titulo = p['titulo']
-        precio = extraer_precio(p['precio'])
-        productos_filtrados.append({'titulo': titulo, 'precio': precio})
-        print("-----------------------------------------------------")
-        print(productos_filtrados)
-
-    df = pd.DataFrame(productos_filtrados)
-    df.to_csv("productos.csv", index=False, encoding="utf-8-sig")
-    print(f"CSV generado con {len(df)} productos")
+    assert not (os.path.exists("productos.csv"))
+    datos = extraer_productos("https://diaonline.supermercadosdia.com.ar/almacen?page=2")
+    assert(len(datos) > 1)
+    assert("titulo" in datos[0])
+    assert("precio" in datos[0])
+    assert (os.path.exists("productos.csv"))
 
 
-    # Convertir a DataFrame y guardar CSV
-    df = pd.DataFrame(productos_filtrados)
-    df.to_csv("productos_limpios.csv", index=False, encoding="utf-8-sig")
-    print(f"CSV generado con {len(df)} productos")
+
+#verificar que la funcion no exista, ejecutar la funcion y verificar que exista
